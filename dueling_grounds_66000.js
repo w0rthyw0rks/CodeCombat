1b1294c41d2f74116533e492350623f52c6f4f3f
Kyle Langworthy <kyle@worthy.works>
2019-02-06 21:27:50 -0500

    Create dueling_grounds_66000.js

---
+++ dueling_grounds_66000.js
@@ -0,0 +1,8 @@
+// Defeat the enemy hero in a duel!
+
+while(true) {
+    // Find and attack the enemy inside a loop.
+    // When you're done, submit to the multiplayer ladder!
+    var enemy = hero.findNearestEnemy();
+    hero.attack(enemy);
+    }
